9e837aafa02d7abb7f9d2d5056b4cad2761949e1
MihirMistry2 <mihirmistry82@gmail.com>
2021-02-13 16:58:19 +0530

    Add create table query and handled database error if table not created now it will create table if not exist.

--- BACKEND/REST_API/src/db/database.js
+++ BACKEND/REST_API/src/db/database.js
@@ -8,9 +8,37 @@ const connection = mysql.createConnection({
     database: process.env.MYSQL_DB,
 });
 
+const queryArrays = [
+    `create table if not exists users(
+        id int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name varchar(255) NOT NULL,
+        email varchar(255) NOT NULL,
+        password varchar(255) NOT NULL,
+        created_at timestamp NOT NULL DEFAULT current_timestamp(),
+        updated_at timestamp NOT NULL DEFAULT current_timestamp()
+    )`,
+
+    `create table if not exists todos(
+        id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        user_id int(11) NOT NULL,
+        description varchar(255) NOT NULL,
+        created_at timestamp NOT NULL DEFAULT current_timestamp(),
+        updated_at timestamp NOT NULL DEFAULT current_timestamp(),
+        CONSTRAINT FK_todos FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
+    )`,
+];
+
 connection.connect((err) => {
     if (err) {
         return console.log('error: ' + err.message);
+    }
+
+    for (let queryArray of queryArrays) {
+        connection.query(queryArray, function (err, results, fields) {
+            if (err) {
+                console.log(err.message);
+            }
+        });
     }
 
     console.log('Connected to the MySQL server.');
